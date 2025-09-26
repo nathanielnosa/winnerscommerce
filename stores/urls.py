184b8ff1d2e08dbd04abf98abd15f5ffa52a943a
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-09-26 11:17:30 +0100

    payment and checkout

--- stores/urls.py
+++ stores/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('addtocart/<str:id>/', views.AddToCartView.as_view()),
     path('mycart/', views.MyCartView.as_view()),
     path('managecart/<str:id>/', views.ManageCartView.as_view()),
+    path('checkout/', views.CheckoutView.as_view()),
+    path('payment/<str:id>/', views.PaymentView.as_view(), name='payment'),
+    path('<str:ref>/', views.VerifyPaymentView.as_view()),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.urls import reverse
+import requests
 from rest_framework.views import APIView
 from rest_framework import status,serializers
 from rest_framework.response import Response
@@ -126,6 +129,12 @@ class AddToCartView(APIView):
             while transaction.atomic():
                 if cart_id:
                     cart = Cart.objects.filter(id=cart_id).first()
+
+                    # assign cart to as logged in user
+                    if request.user.is_authenticated and hasattr(request.user,'profile'):
+                        cart.profile = request.user.profile
+                        cart.save()
+
                     # check if cart exist
                     if cart is None:
                         cart = Cart.objects.create(total=0)
@@ -168,6 +177,10 @@ class MyCartView(APIView):
             cart_id = request.session.get('cart_id',None)
             if cart_id:
                 cart = get_object_or_404(Cart, id=cart_id)
+                # assign cart to as logged in user
+                if request.user.is_authenticated and hasattr(request.user,'profile'):
+                    cart.profile = request.user.profile
+                    cart.save()
                 return Response({"Message": f'Profile - {cart.profile}'}, status=status.HTTP_200_OK)
             return Response({"Message":"Cart not found"}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
@@ -209,3 +222,93 @@ class ManageCartView(APIView):
             return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # ::::: END OF MANAGE CART ::::::
+
+# ::::: CHECKOUT VIEW ::::::
+class CheckoutView(APIView):
+    def post(self, request):
+        try:
+            cart_id = request.session.get('cart_id',None)
+            if cart_id is None:
+                return Response({"error":"You do not have an active cart"},status=status.HTTP_400_BAD_REQUEST)
+            cart_obj = get_object_or_404(Cart, id=cart_id)
+
+            serializers = CheckOutSerializer(data=request.data)
+            if serializers.is_valid():
+                order = serializers.save(
+                    cart = cart_obj,
+                    subtotal = cart_obj.total,
+                    amount = cart_obj.total
+                )
+                del request.session['cart_id']
+                
+                if order.payment_method == "paystack":
+                    payment_url = reverse('payment', args=[order.id])
+                    return Response({"Redirect Payment Url": payment_url}, status=status.HTTP_200_OK)
+                return Response({"Message":"Order created successfully"}, status=status.HTTP_201_CREATED)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# ::::: END OF CHECKOUT VIEW ::::::
+
+# ::::: PAYMENT VIEW ::::::
+class PaymentView(APIView):
+    def get(self, request, id):
+        try:
+            order = get_object_or_404(Order, id=id)
+            if order is None:
+                return Response({"error":"Order not found"},status=status.HTTP_404_NOT_FOUND)
+            
+            url = "https://api.paystack.co/transaction/initialize" 
+            headers = {"Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"}
+            data = {
+                "amount": order.amount * 100, 
+                "email": order.email,
+                "reference": order.ref,
+            }
+            response = requests.post(url, data=data, headers=headers)
+            response_data = response.json()
+
+            if response_data["status"]:
+                paystack_url = response_data["data"]
+                return Response(
+                    {
+                        'order':order.id,
+                        'total':order.amount,
+                        'paystack_public_key':settings.PAYSTACK_PUBLIC_KEY,
+                        'paystack_url':paystack_url
+
+                    }, status=status.HTTP_200_OK
+                )
+            else:
+                return Response({"error":"Payment initialization failed"},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class VerifyPaymentView(APIView):
+    def get(self,request,ref):
+        try:
+            order = get_object_or_404(Order, ref=ref)
+            url = f'https://api.paystack.co/transaction/verify/{ref}'
+            headers =headers = {
+                "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+            }
+            response = requests.get(url,headers=headers)
+            response_data = response.json()
+
+            if response_data["status"] and response_data["data"]["status"] == "success":
+                order.payment_completed = True
+                order.order_status = "completed"
+                order.save()
+                return Response({"Message": "payment was successful"}, status= status.HTTP_200_OK)
+            elif response_data["data"]["status"] == "abandoned":
+                order.order_status = "pending"
+                order.save()
+                return Response({"Message": "payment was abandoned"}, status= status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({"Message":"Payment failed"}, status= status.HTTP_400_BAD_REQUEST)
+        except Order.DoesNotExist:
+            return Response({"Message":"Invalid payment reference"}, status= status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
